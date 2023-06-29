a0b8d3b1b958442b9668f8fb92d03913c9d49acc
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-06-29 22:44:34 +0900

    add routing

--- src/index.js
+++ src/index.js
@@ -3,15 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import AllProducts from './pages/AllProducts';
+import Home from './pages/Home';
+import MyCart from './pages/MyCart';
+import ProductDetail from './pages/ProductDetail';
+import NewProducts from './pages/NewProducts';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, path: '/', element: <Home /> },
+      {path: '/products', element: <AllProducts />},
+      {path: '/products/new', element: <NewProducts />},
+      {path: '/products/:id', element: <ProductDetail />},
+      {path: '/carts', element: <MyCart />}
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
