1d69c6f6a7ea04fda91f0bd4a4da59453835a6f5
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-06-29 22:44:00 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors:{
+        // 변수 색상 지정
+        brand: '#F96162'
+      }
+    },
+  },
+  plugins: [],
+}
+
