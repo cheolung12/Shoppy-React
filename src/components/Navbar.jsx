e0a088ca9a96a97a4f00eb7d2c3e97bde4a34d71
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-07-02 12:10:10 +0900

    apply context and button component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FiShoppingBag } from 'react-icons/fi';
-import { BsFillPencilFill } from 'react-icons/bs';
-import { login } from '../api/firebase';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import User from "./User";
+import Button from "./ui/Button";
+import { useAuthContext } from "../context/AuthContext";
 
 export default function Navbar() {
-    return (
-        <header className='flex justify-between border-b border-gray-300 p-2'>
-            <Link to='/' className='flex items-center text-4xl text-brand'>
-                <FiShoppingBag />
-                <h1>Shoppy</h1>
-            </Link>
-            <nav className='flex items-center gap-4 font-semibold'>
-                <Link to='/products'>Products</Link>
-                <Link to='/carts'>Carts</Link>
-                <Link to='/products/new' className='text-2xl'>
-                    <BsFillPencilFill />
-                </Link>
-                <button onClick={login}>Login</button>
-            </nav>
-        </header>
-    );
-}
+  const {user, login, logout} = useAuthContext();
 
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        {user && <Link to="/carts">Carts</Link>}
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+        {user && <User user={user} />}
+        {!user ? (
+          <Button text={'Login'} onClick={login} />
+        ) : (
+          <Button text={'Logout'} onClick={logout} />
+        )}
+      </nav>
+    </header>
+  );
+}
