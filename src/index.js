13f87c0bd0222b03ea29325310f9c9f43c5b68cd
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-07-02 12:11:00 +0900

    add protectedroute

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from './pages/NotFound';
-import AllProducts from './pages/AllProducts';
-import Home from './pages/Home';
-import MyCart from './pages/MyCart';
-import ProductDetail from './pages/ProductDetail';
-import NewProducts from './pages/NewProducts';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import AllProducts from "./pages/AllProducts";
+import Home from "./pages/Home";
+import MyCart from "./pages/MyCart";
+import ProductDetail from "./pages/ProductDetail";
+import NewProducts from "./pages/NewProducts";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     errorElement: <NotFound />,
     children: [
-      {index: true, path: '/', element: <Home /> },
-      {path: '/products', element: <AllProducts />},
-      {path: '/products/new', element: <NewProducts />},
-      {path: '/products/:id', element: <ProductDetail />},
-      {path: '/carts', element: <MyCart />}
+      { index: true, path: "/", element: <Home /> },
+      { path: "/products", element: <AllProducts /> },
+      {
+        path: "/products/new",
+        element: (
+          <ProtectedRoute requireAdmin>
+            <NewProducts />
+          </ProtectedRoute>
+        ),
+      },
+      { path: "/products/:id", element: <ProductDetail /> },
+      {
+        path: "/carts",
+        element: (
+          <ProtectedRoute>
+            <MyCart />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ]);
