5bea4ba35061763d7e4896611ca4398a97c39159
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-07-23 14:14:02 +0900

    refactor product-query

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,21 +1,20 @@
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { getProducts } from "../api/firebase";
-import ProductCard from "./ProductCard";
+import React from 'react';
+import ProductCard from './ProductCard';
+import useProducts from '../hooks/useProducts';
 
 export default function Products() {
-  // data가 있다면 products라는 이름으로 가져옴
-  const { isLoading, error, data: products } = useQuery([], getProducts);
-
+  const {
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
-      <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+      <ul className='grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4'>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
-          ))} 
+          ))}
       </ul>
     </>
   );
--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import Button from "../components/ui/Button";
-import { uploadImage } from "../api/uploader";
-import { addNewProduct } from "../api/firebase";
+import React, { useState } from 'react';
+import { uploadImage } from '../api/uploader';
+import Button from '../components/ui/Button';
+import useProducts from '../hooks/useProducts';
 
-export default function NewProducts() {
+export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
-    // 이름이 file인 경우에만 file을 set, 나머지 경우에는 업데이트 되는 부분만 덮어씌우기
-    if (name === "file") {
+    if (name === 'file') {
       setFile(files && files[0]);
+      console.log(files[0]);
       return;
     }
     setProduct((product) => ({ ...product, [name]: value }));
@@ -21,53 +22,55 @@ export default function NewProducts() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsUploading(true);
-    // 제품의 사진을 Cloudinary에 업로드하고 url을 획득
-    uploadImage(file)
-    .then(url => {
-      //console.log(url);
-      // firebase에 새로운 제품을 추가
-      addNewProduct(product, url)
-      .then(()=>{
-        setSuccess('성공적으로 제품이 추가되었습니다');
-        setTimeout(()=>{
-          setSuccess(null);
-        }, 4000);
-        setProduct({});
-        setFile(null);
+    uploadImage(file) //
+      .then((url) => {
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess('성공적으로 제품이 추가되었습니다.');
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          }
+        );
       })
-    })
-    // 실패하던 성공하던 일단 버튼 활성화
-    .finally(()=> setIsUploading(false));
+      .finally(() => setIsUploading(false));
   };
+
   return (
     <section className='w-full text-center'>
-      <h2 className='text-2xl font-bold my-4'>새로운 제품등록</h2>
+      <h2 className='text-2xl font-bold my-4'>새로운 제품 등록</h2>
       {success && <p className='my-2'>✅ {success}</p>}
-      {/* createObjectURL : input[type=file]을 통하여 preview 이미지를 만들고 싶을 때 사용 */}
-      {/* mx-auto : 마진을 auto로 주면 중앙정렬 */}
-      {file && <img className='w-96 mx-auto mb-2' src={URL.createObjectURL(file)} alt='local file' />}
+      {file && (
+        <img
+          className='w-96 mx-auto mb-2'
+          src={URL.createObjectURL(file)}
+          alt='local file'
+        />
+      )}
       <form className='flex flex-col px-12' onSubmit={handleSubmit}>
-        {/* image/* : 이미지를 받는데 확장자는 상관 없음 */}
         <input
-          type="file"
-          accept="image/*"
-          name="file"
+          type='file'
+          accept='image/*'
+          name='file'
           required
           onChange={handleChange}
         />
         <input
-          type="text"
-          name="title"
-          value={product.title ?? ""}
-          placeholder="제품명"
+          type='text'
+          name='title'
+          value={product.title ?? ''}
+          placeholder='제품명'
           required
           onChange={handleChange}
         />
         <input
-          type="number"
-          name="price"
-          value={product.price ?? ""}
-          placeholder="가격"
+          type='number'
+          name='price'
+          value={product.price ?? ''}
+          placeholder='가격'
           required
           onChange={handleChange}
         />
@@ -80,18 +83,25 @@ export default function NewProducts() {
           onChange={handleChange}
         />
         <input
-          type="text"
-          name="options"
-          value={product.options ?? ""}
-          placeholder="옵션들(콤마(,)로 구분"
+          type='text'
+          name='description'
+          value={product.description ?? ''}
+          placeholder='제품 설명'
           required
           onChange={handleChange}
         />
-
-        <Button 
-          text={isUploading? '업로드 중...' : '제품 등록하기'}
+        <input
+          type='text'
+          name='options'
+          value={product.options ?? ''}
+          placeholder='옵션들(콤마(,)로 구분)'
+          required
+          onChange={handleChange}
+        />
+        <Button
+          text={isUploading ? '업로드중...' : '제품 등록하기'}
           disabled={isUploading}
-         />
+        />
       </form>
     </section>
   );
